bbe4ff36a8be2a1546d30eed7e606bfa2b63eadf
Wagner Gabriel <yamigabriel@gmail.com>
2020-05-19 15:52:58 -0400

    first commit

---
+++ Navbar/scrips.js
@@ -0,0 +1,17 @@
+//classList - shows/gets all classes
+//contains - checks classList for specific class
+//add - add class
+//remove - remove class
+//togle - toggles class
+
+const navToggle = document.querySelector('.nav-toggle')
+const links = document.querySelector('.links')
+
+navToggle.addEventListener('click', () => {
+    // console.log(links.classList.contains('links'))
+    if (links.classList.contains('show-links')) {
+        links.classList.remove('show-links')
+    } else {
+        links.classList.add('show-links')
+    }
+})
